ffeaa5c7dd1c14440270cd958b731b3c6b6fec2d
ShreyaChopra13 <73791717+ShreyaChopra13@users.noreply.github.com>
2021-04-10 12:11:50 +0530

    Delete website directory

--- website/website/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('users.urls')),
-    path('chat/', include('users.urls')),
-
-]
